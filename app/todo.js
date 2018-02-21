385ca231c857ddf3c32e02cb0f5908f413a58df6
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-02-21 15:19:28 +0530

    Refactored: Template string and css

--- app/todo.js
+++ app/todo.js
@@ -1,16 +1,18 @@
-var myApp = {
+var state = {
   todos: [
     {id: 1, task: 'Get the training done!',status:true},
     {id: 2, task: 'Ensure everyone including me understands this',status:false},
     {id: 3, task: 'Be happy',status: false}
-  ],
+  ]
+};
   
-  refreshUI : function () {
+var myApp = {
+  render : function () {
     var elem = document.getElementById('todos');
     var html = "";
 
-    console.log(this.todos.length);
-    if (this.todos.length === 0) {
+    console.log(state.todos.length);
+    if (state.todos.length === 0) {
         elem.innerHTML = "No todos yet! Be awesome and create some!!!";
       return;
     }
@@ -18,32 +20,37 @@ var myApp = {
 
     var btnStatus = "";
 
-    var btnDelete = "<button type='button' onclick='myApp.removeTodo(this)' class='btn'>remove</button>";
+    var btnDelete = `<button type='button' onclick='myApp.removeTodo(this)' 
+          class='btn'>remove</button>`;
 
-    for(var i = 0; i < this.todos.length; i++) {
-      var todo = this.todos[i];
-      var style = "";
-
-      btnStatus = "<button type='button' onclick='myApp.toggleTodos(this)' class='btn'>complete</button>";
+    for(var i = 0; i < state.todos.length; i++) {
+      let todo = state.todos[i];
+      let style = "";
+      btnStatus = `<button type='button' onclick='myApp.toggleTodos(this)' 
+          class='btn'>complete</button>`;
 
       if (todo.status === true) {
         style = "todo-completed";
-        btnStatus = "<button type='button' onclick='myApp.toggleTodos(this)' class='btn'>undo</button>";
+        btnStatus = `<button type='button' onclick='myApp.toggleTodos(this)' 
+                     class='btn'>undo</button>`;
       }  
-
-      html += "<li id=" + todo.id + " class=" + style + ">" + this.todos[i].task + btnStatus + btnDelete +  "</li>" ;
+      //html += "<li id=" + todo.id + " class=" + style + ">" + this.todos[i].task + btnStatus + btnDelete +  "</li>" ;
+      
+      html += `
+        <li id=${todo.id} class=${style}>
+          ${state.todos[i].task}${btnStatus}${btnDelete}
+        </li>
+      `;
     }
 
     elem.innerHTML = html;
   },
   addTodo: function() {
     var todo = document.getElementById('todo').value;
-
-    this.todos.push({id: this.todos.length+1, task: todo, status:false});
-
+    state.todos.push({id: state.todos.length+1, task: todo, status:false});
+    
     console.log('TODOS: ', this.todos);
-
-    this.refreshUI();
+    this.render();
   },
   toggleTodos: function(el) {
     //e.parentNode.className = "";
@@ -53,15 +60,15 @@ var myApp = {
 
     var todo = {};
 
-    for(var i = 0; i < this.todos.length; i++){
-      todo = this.todos[i];  
+    for(var i = 0; i < state.todos.length; i++){
+      todo = state.todos[i];  
       if (todo.id  == todoId) {
         break;
       }
     }
 
     todo.status = !todo.status;
-    myApp.refreshUI();
+    myApp.render();
   },
   removeTodo:  function(el) {
     el.parentNode.classList.remove("todo-completed");
@@ -70,23 +77,18 @@ var myApp = {
 
     var todoIndex = -1;
 
-    for(var i = 0; i < this.todos.length; i++){
+    for(var i = 0; i < state.todos.length; i++){
       todoIndex = i; 
-      
-      if (this.todos[i].id  == todoId) {
+      if (state.todos[i].id  == todoId) {
         break;
       }
     }
-
-    this.todos.splice(todoIndex, 1);
-
-
-    myApp.refreshUI();
-
+    state.todos.splice(todoIndex, 1);
+    myApp.render();
   }
 };
 
-myApp.refreshUI();
+myApp.render();
 
   
 
